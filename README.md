936e9cc9c89ccdd05076e887cd4a355cb085a111
jed76 <jakedalzell2@gmail.com>
2025-03-04 11:54:32 +0000

    improvements to calculating phylogenetic rarity

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project aims to predict properties of host-symbiont networks based on host ecology and population structure. It uses data from the BSBI Plant Atlas 2020<sup>1,2</sup>, the Daphne dated phylogeny of the European Flora<sup>3</sup>, and the BRC Database of Insects and their Food Plants<sup>4</sup>.
 
-1.  P. A. Stroh, K. J. Walker, T. A. Humphrey, O. L. Pescott, R. J. Burkmar, Plant Atlas 2020 --- British and Irish vascular plant and charophyte 10 x 10 km grid square locations, subdivided by survey period, up to 2019, version 1, Zenodo (2024); <https://doi.org/10.5281/zenodo.10998003>.
+1.  P. A. Stroh, K. J. Walker, T. A. Humphrey, O. L. Pescott, R. J. Burkmar, Plant Atlas 2020 — British and Irish vascular plant and charophyte 10 x 10 km grid square locations, subdivided by survey period, up to 2019, version 1, Zenodo (2024); <https://doi.org/10.5281/zenodo.10998003>.
 2.  P. A. Stroh, K. J. Walker, T. A. Humphrey, O. L. Pescott, R. J. Burkmar, Plant Atlas 2020: Mapping Changes in the Distribution of the British and Irish Flora. (Princeton University Press and Botanical Society of Britain and Ireland, 2023).
-3.  W. Durka, S. G. Michalski, Daphne: a dated phylogeny of a large European flora for phylogenetically informed ecological analyses. Ecology 93, 2297--2297 (2012); <https://doi.org/10.1890/12-0743.1>
+3.  W. Durka, S. G. Michalski, Daphne: a dated phylogeny of a large European flora for phylogenetically informed ecological analyses. Ecology 93, 2297–2297 (2012); <https://doi.org/10.1890/12-0743.1>
 4.  Database of Insects and their Food Plants, British Records Centre. <https://dbif.brc.ac.uk/homepage.aspx>.
--- R_scripts/25_03_03 8 phylogenetic rarity in a square.R
+++ R_scripts/25_03_03 8 phylogenetic rarity in a square.R
@@ -1,8 +1,85 @@
+library(stringr)
 
 nameLookup <- read.csv("data/BSBI_PA2020_10by10kmSquareData/nameLookup.csv")
+taxonlist <- unique(nameLookup$ddbidLookup)
+
+
 taxon <- nameLookup$ddbidLookup[1] # take ID of first taxon in database
 taxon <- "2cd4p9h_13q"
 
 taxonDistribution <- read.csv(paste("data/BSBI_PA2020_10by10kmSquareData/hectadDistributions/", taxon, ".csv", sep=""))
 
 taxonDistribution$hectad # list of hectads where taxon is present
+
+
+# add phylodistance taxon names to lookup list ----------------------------
+
+nameLookup$phyloLookup <- sub(" ", "_", nameLookup$taxonName)
+
+
+
+# get list of squares -----------------------------------------------------
+taxon <- "2cd4p9h_xbs" # Bellis perennis
+taxonDistribution <- read.csv(paste("data/BSBI_PA2020_10by10kmSquareData/hectadDistributions/", taxon, ".csv", sep=""))
+
+hectadlist <- unique(taxonDistribution$hectad)
+
+
+# taxon list for a square -------------------------------------------------
+
+hectad <- "X99"
+hectadSpeciesList <- c()
+counter = 0
+for(taxon in taxonlist){
+  print(paste(counter, "out of 3497", sep = " "))
+  taxonDistribution <- read.csv(paste("data/BSBI_PA2020_10by10kmSquareData/hectadDistributions/", taxon, ".csv", sep=""))
+  
+  hectadSpeciesList <- rbind(hectadSpeciesList, c(taxon, hectad %in% taxonDistribution$hectad))
+  counter = counter + 1
+}
+write.csv(hectadSpeciesList, paste("data/BSBI_PA2020_10by10kmSquareData/hectadSpeciesLists/", hectad, ".csv", sep=""))
+
+
+# taxon list for all squares ----------------------------------------------
+
+hectadlist <- hectadlist[-c(1:10)] # remove hectads that we have already done
+hectadlistlength <- length(hectadlist)
+
+hectadCounter <- 1
+for(hectad in hectadlist){
+  hectadSpeciesList <- c()
+  taxonCounter = 1
+  for(taxon in taxonlist){
+    if(taxonCounter %% 100 == 0){
+      print(paste(taxonCounter, "out of 3497 taxa", sep = " "))
+    }
+    taxonDistribution <- read.csv(paste("data/BSBI_PA2020_10by10kmSquareData/hectadDistributions/", taxon, ".csv", sep=""))
+    
+    if(hectad %in% taxonDistribution$hectad){ # if present, include abundance (number of distinct hectads)
+      hectadSpeciesList <- rbind(hectadSpeciesList, c(taxon, TRUE, taxonDistribution[taxonDistribution$hectad == hectad,]$distinct.tetrads))
+    }
+    else{ # if absent, abundance = 0
+      hectadSpeciesList <- rbind(hectadSpeciesList, c(taxon, FALSE, 0))
+    }
+    taxonCounter = taxonCounter + 1
+  }
+  write.csv(hectadSpeciesList, paste("data/BSBI_PA2020_10by10kmSquareData/hectadSpeciesLists/", hectad, ".csv", sep=""))  
+  hectadCounter = hectadCounter + 1
+  print(paste(hectadCounter, "out of", hectadlistlength, "hectads", sep = " "))
+}
+
+# for a given taxon, calculate phylogenetic rarity in a square ------------
+taxon <- "2cd4p9h_xq4"
+
+B60 <- read.csv("data/BSBI_PA2020_10by10kmSquareData/hectadSpeciesLists/B60.csv")
+B60 <- B60[B60$V2,] # remove absent taxa
+# is taxon present?
+taxon %in% B60$V1
+
+taxon1_phylo_matrix_name <- nameLookup[nameLookup$ddbidLookup == taxon,]$phyloLookup
+
+for(taxon2 in B60$V1){
+  taxon2_phylo_matrix_name <- nameLookup[nameLookup$ddbidLookup == taxon2,]$phyloLookup
+  print(paste(taxon1_phylo_matrix_name, taxon2_phylo_matrix_name))
+  print(phylo_distance_matrix[taxon1_phylo_matrix_name, taxon2_phylo_matrix_name])
+}
